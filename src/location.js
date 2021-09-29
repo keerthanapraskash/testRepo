84aced34879be9b396a25853bac4ddc129be5273
keerthanapraskash <keerthanaprakash7403@gmail.com>
2021-09-29 20:41:28 +0530

    added locaton.js

---
+++ src/location.js
@@ -0,0 +1,13 @@
+const geoCode = (address,callback)=>{
+    setTimeout(()=>{
+        const data = {
+            latitude : "200",
+            longituee : "300"
+        }
+        callback(data);
+    },2000);
+}
+
+geoCode("Alappuzha",(data)=>{
+    console.log(data);
+});
